800ce8bba38968e43afb35aabc8ef94f74a99265
Uriel Aispuro <urielalexis64@gmail.com>
2021-07-09 23:45:58 -0600

    remove Container widget

--- lib/pages/login/widgets/welcome.dart
+++ lib/pages/login/widgets/welcome.dart
@@ -13,54 +13,53 @@ class Welcome extends StatelessWidget {
       aspectRatio: 16 / 11,
       child: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
-          return Container(
-            child: Stack(
-              children: [
-                Positioned(
-                  top: constraints.maxHeight * .7,
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 3,
-                        width: constraints.maxWidth,
-                        color: const Color(0xffeeeeee),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        'Welcome Back!',
-                        style: TextStyle(
-                            fontSize: responsive.ip(2.5),
-                            fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ),
-                ),
-                Positioned(
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    child: SvgPicture.asset(
-                      'assets/pages/login/clouds.svg',
+          return Stack(
+            children: [
+              Positioned(
+                top: constraints.maxHeight * .7,
+                child: Column(
+                  children: [
+                    Container(
+                      height: 3,
                       width: constraints.maxWidth,
-                      height: constraints.maxHeight * .7,
-                    )),
-                Positioned(
-                    top: constraints.maxHeight * .285,
-                    child: SvgPicture.asset(
-                      'assets/pages/login/woman.svg',
-                      width: constraints.maxWidth * .35,
-                    )),
-                Positioned(
-                    top: constraints.maxHeight * .31,
-                    right: constraints.maxWidth * .05,
-                    child: SvgPicture.asset(
-                      'assets/pages/login/man.svg',
-                      width: constraints.maxWidth * .26,
-                    ))
-              ],
-            ),
+                      color: const Color(0xffeeeeee),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Welcome Back!',
+                      style: TextStyle(
+                          fontSize: responsive.ip(2.5),
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'raleway'),
+                    )
+                  ],
+                ),
+              ),
+              Positioned(
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  child: SvgPicture.asset(
+                    'assets/pages/login/clouds.svg',
+                    width: constraints.maxWidth,
+                    height: constraints.maxHeight * .7,
+                  )),
+              Positioned(
+                  top: constraints.maxHeight * .285,
+                  child: SvgPicture.asset(
+                    'assets/pages/login/woman.svg',
+                    width: constraints.maxWidth * .35,
+                  )),
+              Positioned(
+                  top: constraints.maxHeight * .31,
+                  right: constraints.maxWidth * .05,
+                  child: SvgPicture.asset(
+                    'assets/pages/login/man.svg',
+                    width: constraints.maxWidth * .26,
+                  ))
+            ],
           );
         },
       ),
