a1000ae539573140c87f682361005519914aa0b9
Uriel Aispuro <urielalexis64@gmail.com>
2021-07-10 17:23:27 -0600

    add scroll and unfocus when click outside

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:login_responsive/pages/login/widgets/login_form.dart';
 import 'package:login_responsive/pages/login/widgets/welcome.dart';
+import 'package:login_responsive/utils/responsive.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key key}) : super(key: key);
@@ -19,13 +20,25 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    final responsive = Responsive.of(context);
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [Welcome(), LoginForm()],
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.white,
+          child: SingleChildScrollView(
+            child: Container(
+              height: responsive.height,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [Welcome(), LoginForm()],
+              ),
+            ),
+          ),
         ),
       ),
     );
