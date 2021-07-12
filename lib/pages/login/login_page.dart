1a225e71544bea7f82b237bf5fee8146912d1f1f
Uriel Aispuro <urielalexis64@gmail.com>
2021-07-11 20:50:57 -0600

    add OrientationBuilder, block landscape on smartphones

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:login_responsive/pages/login/widgets/login_form.dart';
 import 'package:login_responsive/pages/login/widgets/welcome.dart';
 import 'package:login_responsive/utils/responsive.dart';
+import 'package:after_layout/after_layout.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key key}) : super(key: key);
@@ -11,7 +12,7 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage> with AfterLayoutMixin {
   @override
   void initState() {
     super.initState();
@@ -27,20 +28,49 @@ class _LoginPageState extends State<LoginPage> {
           FocusScope.of(context).unfocus();
         },
         child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Colors.white,
-          child: SingleChildScrollView(
-            child: Container(
-              height: responsive.height,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [Welcome(), LoginForm()],
-              ),
-            ),
-          ),
-        ),
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.white,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: OrientationBuilder(
+              builder: (context, orientation) {
+                if (orientation == Orientation.portrait) {
+                  return SingleChildScrollView(
+                    child: Container(
+                      height: responsive.height,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [Welcome(), LoginForm()],
+                      ),
+                    ),
+                  );
+                }
+                return Row(
+                  children: [
+                    Expanded(
+                        child: SingleChildScrollView(
+                            physics: const NeverScrollableScrollPhysics(),
+                            child: SizedBox(
+                                height: responsive.height, child: Welcome()))),
+                    SafeArea(
+                      child: Expanded(
+                          child: SingleChildScrollView(
+                              child: Center(child: LoginForm()))),
+                    )
+                  ],
+                );
+              },
+            )),
       ),
     );
+  }
+
+  @override
+  void afterFirstLayout(BuildContext context) {
+    final bool isTablet = MediaQuery.of(context).size.shortestSide >= 600;
+    if (!isTablet) {
+      // if smartphone
+      // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    }
   }
 }
